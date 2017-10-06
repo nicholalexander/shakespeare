48dff4c82b17a65d2873c124193c65b6687ff9b3
nichol alexander <nichol.alexander@gmail.com>
2017-10-06 16:09:47 -0400

    better sonnets

---
+++ outputter.rb
@@ -0,0 +1,26 @@
+require 'erb'
+require 'YAML'
+
+class Sonnet
+  attr_accessor :number, :lines
+  def initialize(number, lines)
+    @number = number
+    @lines = lines
+  end
+end
+
+file = File.open('sonnet_template.erb')
+template = file.read
+
+@sonnets = YAML.load_file('sonnets.yml')
+
+@sonnets.each do |sonnet|
+  @number = sonnet.number
+  @lines = sonnet.lines
+  output = ERB.new(template).result
+  File.write("sonnets_output/#{@number}.html.erb", output)
+end
+
+
+
+
